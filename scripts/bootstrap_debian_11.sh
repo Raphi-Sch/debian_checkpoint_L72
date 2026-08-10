5fc8f7fbdce67e940e688516196ca37c6ae1eb1d
Raphi-Sch <>
2026-08-10 17:00:33 +0200

    update

--- scripts/bootstrap_debian_11.sh
+++ scripts/bootstrap_debian_11.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+mkdir ../debian11-armhf
+
 sudo debootstrap \
   --arch=armhf \
   --foreign \
-  --include=kmod,net-tools,iproute2,openssh-server,nano,wget,curl,bash-completion \
+  --include=nano,openssh-server,kmod,wget,bash-completion \
   bullseye \
-  ../debian-armhf \
+  ../debian11-armhf \
   http://deb.debian.org/debian
 
 
 # Copy QEMU ARM binary into the chroot so ARM ELFs can execute
-sudo cp /usr/bin/qemu-arm-static ../debian-armhf/usr/bin/
+sudo cp /usr/bin/qemu-arm-static ../debian11-armhf/usr/bin/
 
 # Enter the chroot and finish debootstrap
-sudo chroot ../debian-armhf /usr/bin/qemu-arm-static /bin/bash -c "/debootstrap/debootstrap --second-stage"
+sudo chroot ../debian11-armhf /usr/bin/qemu-arm-static /bin/bash -c "/debootstrap/debootstrap --second-stage"
