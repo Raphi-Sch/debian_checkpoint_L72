e549bf17bec11ee3550f1121ee468bfc14417a86
administrateur <administrateur@PE5299-00071>
2026-08-10 16:21:39 +0200

    Initial commit

---
+++ scripts/mount_disk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Mounting 'boot' on '/mnt/cpboot'"
+sudo mount --uuid 6ca4ef5f-335d-4efe-8b47-bef2fc10360e /mnt/cpboot
+
+echo "Mounting 'debian' on '/mnt/cpdebian'"
+sudo mount --uuid 765fecd2-9eae-4d19-afd5-42c11e07b76f /mnt/cpdebian
---
+++ scripts/umount_disk.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Unmounting '/mnt/cpboot'"
+sudo umount /mnt/cpboot
+
+echo "Unmounting '/mnt/cpdebian'"
+sudo umount /mnt/cpdebian
