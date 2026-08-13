b60c3e99a499fad26b7e381b007a87e654bdf27f
Raphael <raphael.scharff@gmail.com>
2026-08-13 17:48:18 +0200

    fix path

--- scripts/build_initramfs_standalone.sh
+++ scripts/build_initramfs_standalone.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 echo "Compressing initramfs..."
-(cd ../kernel/initramfs; sudo find . | sudo cpio -H newc -o | gzip -9 > ./_initramfs.gz)
+(cd ../kernel/initramfs; sudo find . | sudo cpio -H newc -o | gzip -9 > ../_initramfs.gz)
 
 echo "Packing initramfs..."
-cd ../kernel/initramfs
-mkimage -A arm -O linux -T ramdisk -C gzip -n "Custom Initramfs" -d ./_initramfs.gz ./_initramfs.uimg
+cd ../kernel/
+mkimage -A arm -O linux -T ramdisk -C gzip -n "Custom Initramfs" -d _initramfs.gz initramfs.uimg
 
 rm ./_initramfs.gz 
 
 while true; do
     read -p "Do you want to copy 'initramfs.uimg' to '/srv/tftp' [Y/N] : " yn
     case $yn in
-        [Yy]* ) echo "Copying file to tftp..."; sudo cp -v ./_initramfs.uimg /srv/tftp/initramfs.uimg; break;;
+        [Yy]* ) echo "Copying file to tftp..."; sudo cp -v ./initramfs.uimg /srv/tftp/initramfs.uimg; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no;";;
     esac
