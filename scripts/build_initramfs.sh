7a74326ea2188f39bafa498f5f2fbf8710d50af8
Raphael <raphael.scharff@gmail.com>
2026-08-15 18:43:58 +0200

    remove sudo

--- scripts/build_initramfs.sh
+++ scripts/build_initramfs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Compressing initramfs..."
-(cd ../kernel/initramfs; sudo find . | sudo cpio -H newc -o | gzip -9 > ../_initramfs.gz)
+(cd ../kernel/initramfs; find . | cpio -H newc -o | gzip -9 > ../_initramfs.gz)
 
 echo "Packing initramfs..."
 cd ../kernel/
@@ -12,7 +12,7 @@ rm ./_initramfs.gz
 while true; do
     read -p "Do you want to copy 'initramfs.uimg' to '/srv/tftp' [Y/N] : " yn
     case $yn in
-        [Yy]* ) echo "Copying file to tftp..."; sudo cp -v ./initramfs.uimg /srv/tftp/initramfs.uimg; break;;
+        [Yy]* ) echo "Copying file to tftp..."; cp -v ./initramfs.uimg /srv/tftp/initramfs.uimg; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no;";;
     esac
